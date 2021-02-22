4ebf04e525efdfae5e3407ca09eff740fed188f6
Илья <72180656+Eliss-good@users.noreply.github.com>
2021-02-22 17:20:47 +0400

    2.1

--- simvol-1.cpp
+++ simvol-1.cpp
@@ -1,1 +1,17 @@
-simvol-1.cpp
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	int Q;
+	int sum = 0;
+	char p;
+	cin >> Q;
+	for(int i = 0; i < Q; ++i){
+        cin >> p;
+        sum += (int)(p) - 48;
+
+    }
+    cout << sum; 
+	return 0;
+}
