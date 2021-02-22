30f0fea17afee2334c3c8b336f2a16b73af226b0
Илья <72180656+Eliss-good@users.noreply.github.com>
2021-02-22 23:10:47 +0400

    2.3

--- simvol-3.cpp
+++ simvol-3.cpp
@@ -4,19 +4,19 @@
 using namespace std;
 
 int main(){
-	int sum = 0;
+    int sum = 0;
     int q;
     cout << "Ведите кол-во символов" << endl;
     cin >> q;
     char simvol;
     for(int i = 0; i < q; ++i){
-    	cin >> simvol;
-    	if((simvol >= '0') && (simvol <= '9')){
-    		sum += (int)simvol - 48;
-    	} else {
-            cout << tolower(simvol);
-    	}
-    	
+        cin >> simvol;
+        if((simvol >= '0') && (simvol <= '9')){
+            sum += (int)simvol - 48;
+        } else {
+            cout << (char)tolower(simvol);
+        }
+        
     }
     cout << "Cумма чисел : " << sum << endl;
     return 0;
